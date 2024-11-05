0c06bc74912d3ac2469ec7328c7e2a2504831a61
orcunakkaya <berkayakkaya49@gmail.com>
2024-11-05 15:31:26 +0300

    collection page added

---
+++ src/app/collection/[id]/page.jsx
@@ -0,0 +1,8 @@
+
+const Home = async ({ params }) => {
+    return (
+        <div>burası collection id</div>
+    )
+}
+
+export default Home
--- src/components/LibraryList.jsx
+++ src/components/LibraryList.jsx
@@ -1,12 +1,19 @@
 'use client'
 import React, { useState } from 'react'
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const LibraryList = () => {
+    const router = useRouter();
     const [isOpen, setIsOpen] = useState(false);
+
+    const handleRouter = (route) => {
+        router.push(`/collection/${route}`);
+    }
+
   return (
 
-        <button className={`block min-w-full h-16 p-2 border border-transparent ${!isOpen && 'hover:bg-hoverBackgroundColor'} rounded ${isOpen && 'bg-tinted text-white'}`}>
+        <button onClick={() => handleRouter('liked-songs')} className={`block min-w-full h-16 p-2 border border-transparent ${!isOpen && 'hover:bg-hoverBackgroundColor'} rounded ${isOpen && 'bg-tinted text-white'}`}>
           <div className='h-12 flex flex-nowrap gap-2'>
             <div className='border-none rounded h-12 min-w-12 grid place-items-center' style={{backgroundColor: '(#5018F0, #282828)'}}>
                 <Image 
@@ -20,7 +27,7 @@ const LibraryList = () => {
             </div>
             <div className='grid gap-0.5 text-left'>
                 <span>Beğenilen Şarkılar</span>
-                <span>0 Şarkı</span>
+                <span className='text-subdued'>0 Şarkı</span>
             </div>
           </div>
     </button>
